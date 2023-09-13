90433757ae78090250c01226c482b16c0fef7209
Guilherme Rocha <guirocha_2003@hotmail.com>
2023-09-13 15:08:50 -0300

    a05(repeticao): declaracao de variaveis

--- TesteSegundaRepeticao.java
+++ TesteSegundaRepeticao.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class TesteSegundaRepeticao {
-    
+    public static void main(String[] args) {
+        int quantosAlunos = Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade de alunos"));
+
+        int contator = 1;
+        Scanner leitor = new Scanner(System.in);
+        while (contator <= quantosAlunos) {
+
+            double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Digite sua primeira nota"));
+            double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Digite sua segunda nota"));
+            double media;
+
+            media = (nota1 + nota2) / 2;
+
+            if (media >= 7) {
+
+                JOptionPane.showMessageDialog(null, "Você foi aprovado com essa nota " + media);
+            }else if (media == 4 || media <= 6){
+                JOptionPane.showMessageDialog(null, "Vai para recuperação com essa nota " + media);
+            } else {
+                JOptionPane.showMessageDialog(null, "Você foi reprovado");
+            }
+            
+            contator = contator + 1;
+
+        }
+        leitor.close();
+    }
 }
